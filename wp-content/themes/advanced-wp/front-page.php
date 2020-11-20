7250fd292c66bb83c0e753cb9be89dccfd41c167
Dan <souzadanbr@gmail.com>
2020-11-19 18:13:47 -0800

    Adding more features by Daniel Souza, next commit will send the jquery responsive menu

--- wp-content/themes/advanced-wp/front-page.php
+++ wp-content/themes/advanced-wp/front-page.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
 
 <div class="container content">
+ <?php if(is_active_sidebar('showcase')) : ?>
+ <?php dynamic_sidebar('showcase'); ?>
+ <?php endif ?>
  <div class="main block">
   <?php if (have_posts()) : ?>
   <?php while (have_posts()) : the_post(); ?>
@@ -34,12 +37,22 @@
  </div>
 
  <div class="side">
-		<?php if(is_active_sidebar('sidebar')) : ?>
-			<?php dynamic_sidebar('sidebar'); ?>
-			<?php endif ?>
+  <?php if(is_active_sidebar('sidebar')) : ?>
+  <?php dynamic_sidebar('sidebar'); ?>
+  <?php endif ?>
  </div>
+ <?php if(is_active_sidebar('box1')) : ?>
+ <?php dynamic_sidebar('box1'); ?>
+ <?php endif ?>
+ <?php if(is_active_sidebar('box2')) : ?>
+ <?php dynamic_sidebar('box2'); ?>
+ <?php endif ?>
+ <?php if(is_active_sidebar('box3')) : ?>
+ <?php dynamic_sidebar('box3'); ?>
+ <?php endif ?>
 </div>
 <?php get_footer() ?>
 
 </body>
+
 </html>
--- wp-content/themes/advanced-wp/functions.php
+++ wp-content/themes/advanced-wp/functions.php
@@ -11,12 +11,64 @@ function adv_theme_support()
         'primary'   => __('Primary Menu'),
         'footer'    => __('Footer Menu')
     ));
-// Post Format Support
-add_theme_support('post-formats', array('aside', 'gallery', 'link'));
+    // Post Format Support
+    add_theme_support('post-formats', array('aside', 'gallery', 'link'));
 
 }
 
 add_action('after_setup_theme', 'adv_theme_support');
+
+// Widgets locations
+function init_widgets($id){
+    register_sidebar(array(
+        'name' => 'Sidebar',
+        'id' => 'sidebar',
+        'before_widget' => '<div class="block side-widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'
+    ));
+
+    register_sidebar(array(
+        'name' => 'Showcase',
+        'id' => 'showcase',
+        'before_widget' => '<div class="showcase">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'
+    )); 
+
+    register_sidebar(array(
+        'name' => 'Box 1',
+        'id' => 'box1',
+        'before_widget' => '<div class="box box1">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'
+    )); 
+   
+    register_sidebar(array(
+        'name' => 'Box 2',
+        'id' => 'box2',
+        'before_widget' => '<div class="box box2">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'
+    )); 
+
+    register_sidebar(array(
+        'name' => 'Box 3',
+        'id' => 'box3',
+        'before_widget' => '<div class="box box3">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'
+    )); 
+   
+      
+     
+}
+add_action('widgets_init', 'init_widgets'); // hook to initialize widgets
 
 // Excerpt Lenght
 function adv_set_excerpt_length()
@@ -39,3 +91,4 @@ function page_is_parent(){
     $pages = get_pages('child_of='.$post->ID);
     return count($pages);
 }
+
